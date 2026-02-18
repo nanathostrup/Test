5e5db9e97901770aaace3198ec46b3b9e6219971
nanathostrup <trp313@alumni.ku.dk>
2026-02-18 14:02:32 +0100

    :)

--- test.cs
+++ test.cs
@@ -1,4 +1,5 @@
 public class Config {
+    new string API_SECRET = new string(); 
     public static final String API_SECRET = "MySuperSecretKey";  // This would be flagged by the secret scanner
 }
 
