585c3493858e4955d37483c0507f0ea6fe925bc5
nanathostrup <trp313@alumni.ku.dk>
2026-02-18 14:20:50 +0100

    :)

--- TestData/Program.cs
+++ TestData/Program.cs
@@ -1,9 +1,12 @@
 ﻿class Program
 {
     public string password;
+    public string discord_client_secret;
     static void Main(string[] args)
     {
         password = "MinHundErSåSød";
+        discord_client_secret = "8dyfuiRyq=vVc3RRr_edRk-fK__JItpZ";
+
     }
 }
 
