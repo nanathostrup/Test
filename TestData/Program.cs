1744d4c274692f686d501aa5e9e8afdda0be6d1d
nanathostrup <trp313@alumni.ku.dk>
2026-02-18 14:52:57 +0100

    :)

--- TestData/Program.cs
+++ TestData/Program.cs
@@ -2,10 +2,12 @@
 {
     public string password;
     public string discord_client_secret;
+    public string token;
     static void Main(string[] args)
     {
         password = "MinHundErSåSød";
         discord_client_secret = "8dyfuiRyq=vVc3RRr_edRk-fK__JItpZ";
+        string token = "AKIAIOSFODNN7EXAMPLE";
 
     }
 }
