805cf8b3a1d5612bd67318b31d4af61673a635df
nanathostrup <trp313@alumni.ku.dk>
2026-02-18 15:05:17 +0100

    :)

--- TestData/Program.cs
+++ TestData/Program.cs
@@ -1,13 +1,17 @@
-﻿class Program
+﻿using System.Threading.Tasks;
+
+class Program
 {
     public string password;
     public string discord_client_secret;
     public string token;
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         password = "MinHundErSåSød";
         discord_client_secret = "8dyfuiRyq=vVc3RRr_edRk-fK__JItpZ";
-        string token = "AKIAIOSFODNN7EXAMPLE";
+        string token = "AKIAIOSFODNN7EXAMPLX";
+        string mysecret = "Z8x!9Qa@K2m#Lp7$RvT5wYc";
+        string aws_access_key_id = AKIAXYZDQCEN4B6JSJQI;
 
     }
 }
--- test.cs
+++
@@ -1,5 +0,0 @@
-public class Config {
-    public static new string API_SECRET = new string(); 
-    API_SECRET = "MySuperSecretKey";  // This would be flagged by the secret scanner
-}
-
