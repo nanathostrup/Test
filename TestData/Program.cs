617407393d281f5a2ca9c9d33bc808c442ada014
Nana Thostrup Davidsen <natd@netcompany.com>
2026-02-19 09:05:13 +0100

    update dotnet version

--- TestData/Program.cs
+++ TestData/Program.cs
@@ -2,17 +2,17 @@
 
 class Program
 {
-    public string password;
-    public string discord_client_secret;
-    public string token;
+    public string password  = "";
+    public string discord_client_secret = "";
+    public string token = "";
     static async Task Main(string[] args)
     {
-        password = "MinHundErSåSød";
-        discord_client_secret = "8dyfuiRyq=vVc3RRr_edRk-fK__JItpZ";
+        string password = "MinHundErSåSød";
+        string discord_client_secret = "8dyfuiRyq=vVc3RRr_edRk-fK__JItpZ";
         string token = "AKIAIOSFODNN7EXAMPLX";
         string mysecret = "Z8x!9Qa@K2m#Lp7$RvT5wYc";
-        string aws_access_key_id = AKIAXYZDQCEN4B6JSJQI;
-
+        string aws_access_key_id = "AKIAXYZDQCEN4B6JSJQI";
+        Console.WriteLine(":)");
     }
 }
 
