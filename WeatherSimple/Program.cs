1463decc31bcacfaa5d0a2f0a791b1151febd11c
Nana Thostrup Davidsen <natd@netcompany.com>
2026-03-12 10:39:23 +0100

    pushing all just in case

--- WeatherSimple/Program.cs
+++ WeatherSimple/Program.cs
@@ -6,25 +6,7 @@
         {
             Console.WriteLine("=== Weather Station ===");
 
-            var authService = new AuthService();
             var weatherService = new WeatherStation.WeatherServices();
-
-            Console.Write("Username: ");
-            string? username = Console.ReadLine();
-
-            Console.Write("Password: ");
-            string? password = Console.ReadLine();
-
-            if (username == null || password == null)
-            {
-                return;
-            }
-
-            if (!authService.Login(username, password))
-            {
-                Console.WriteLine("Authentication failed.");
-                return;
-            }
 
             Console.Write("Enter city: ");
             string? city = Console.ReadLine();
@@ -36,7 +18,7 @@
             string? weather = await weatherService.GetWeatherAsync(city);
             Console.WriteLine(weather);
 
-            await AuthService.LoginAsync(); //Selv om funktion ikke virker testes det om den bliver detected når "i brug".
+            string? test = await weatherService.ALittleTest(city); //Bare for at teste når funktionen bliver kaldt
         }
     }
 }
--- WeatherSimple/Services/WeatherServices.cs
+++ WeatherSimple/Services/WeatherServices.cs
@@ -6,11 +6,13 @@ namespace WeatherStation
 {
     public class WeatherServices
     {
-        // Hardcoded API key in env file
-        string? apiKey = Environment.GetEnvironmentVariable("MY_API_KEY");
+        string? apiKey = Environment.GetEnvironmentVariable("MY_API_KEY"); // Hardcoded API key in env file
+        string? defaultCity = Environment.GetEnvironmentVariable("WEATHER_DEFAULT_CITY"); // Non-secret - Test to differntiate between actual secret and non secret
+
         private readonly HttpClient _httpClient = new HttpClient();
-        public async Task<string> GetWeatherAsync(string city)
+        public async Task<string> GetWeatherAsync(string city) // Testing with a hardcoded secret in the env file
         {
+            city ??= defaultCity;
             string url =
              $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric"; // Actually working website fetching weather forecast
             try
